8ce6b0b34aecd95dbd16539130c804810d6afeef
David Kotaev <davidkotaev@gmail.com>
2020-02-23 21:32:40 -0500

    working frontend response

--- middle.php
+++ middle.php
@@ -17,66 +17,77 @@ function headers_to_array($header_text){
   return $headers;
 }
 
+function fields_to_str($fields){
+  $fields_string = "";
+  foreach($fields as $key=>$value){
+    $fields_string .= $key. '=' .$value. '&';
+  }
+  rtrim($fields_string, '&');
+  return $fields_string;
+}
+
 function check_credentials($user, $pass){
     $fail_string = "Unsuccessful login with UCID(%s).";
     $success_string = "Successful login with UCID(%s).";
-    $service_url = 'https://webauth.njit.edu:443//idp/profile/cas/login?service=https%3A%2F%2Fportal.njit.edu%2Fc%2Fportal%2Flogin';
-    // first send request to get to login page
+    $webauth_url = 'https://myhub.njit.edu/vrs/ldapAuthenticateServlet';
+    $fields = array(
+      "user_name" => urlencode($user),
+      "passwd" => urlencode($pass),
+      "SUBMIT" => urlencode('Login')
+    );
     $curl = curl_init();
     curl_setopt_array($curl, array(
-      CURLOPT_URL => $service_url,
-      CURLOPT_FOLLOWLOCATION => true,
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_HEADER => true,
-      CURLOPT_NOBODY => true
-      //  CURLOPT_VERBOSE => true,
-    ));
-    // get webauth url from response header
-    $service_response = curl_exec($curl);
-    $headers = headers_to_array($service_response);
-    $webauth_url = sprintf("https://webauth.njit.edu%s", $headers[0]['Location']);
-    // now send actual request
-    $fields = array(
-      "j_username" => urlencode($user),
-      "j_password" => urlencode($pass),
-      "_eventId_proceed" => ""
-    );
-    curl_setopt_array($curl, array(
       CURLOPT_URL => $webauth_url,
-      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_POST => count($fields),
       CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_HEADER => true,
-      CURLOPT_NOBODY => true,
-      //  CURLOPT_VERBOSE => true,
-      CURLOPT_POST => true,
-      CURLOPT_POSTFIELDS => $fields
+      //CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_POSTFIELDS => fields_to_str($fields)
     ));
     $webauth_response = curl_exec($curl);
-    $webauth_headers = headers_to_array($webauth_response);
-    print_r($webauth_headers);
+    // on success the response will be a redirect, so empty`
+    if (empty($webauth_response)){
+      return true;
+    }
+    // otherwise you get the login page backend
+    else {
+      return false;
+    }
 }
 
-function backend_response($url){
-   $curl = curl_init();
-    curl_setopt_array($curl, array(
-      CURLOPT_URL => $url,
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_VERBOSE => true,
-      CURLOPT_POST => true
-    ));
-    return curl_exec($url);
+function query_backend($url){
+  $curl = curl_init();
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => $url,
+    //CURLOPT_VERBOSE => true,
+    //CURLOPT_POST => true,
+    CURLOPT_RETURNTRANSFER => true
+  ));
+  $response = curl_exec($curl);
+  return $response;
+}
+
+function login_response_json($backend_response, $webauth_bool, $ucid){
+  $fail_string = sprintf("Unsuccessful login with %s (webnjit).", $ucid);
+  $success_string = sprintf("Successful login with %s (webnjit).", $ucid);
+  $copy = json_decode($backend_response, true);
+  $copy['webnjit']['success'] = $webauth_bool;
+  $copy['webnjit']['message'] = ($webauth_bool ? $success_string : $fail_string);
+  return json_encode($copy);
 }
 
 //input from front end
 $frontend_input = file_get_contents('php://input');
 $frontend_data = json_decode($frontend_input, true);
-//$user = $frontend_data['username'];
-//$pass = $frontend_data['password'];
-$user = $argv[1];
-$pass = $argv[2];
+$user = $frontend_data['username'];
+$pass = $frontend_data['password'];
+
+// for testing on command line
+//$user = $argv[1];
+//$pass = $argv[2];
 
 $backend_url = 'https://web.njit.edu/~npm26/logingate.php';
+$frontend_msg = login_response_json(query_backend($backend_url), check_credentials($user, $pass), $user);
+header('Content-Type: application/json');
+echo $frontend_msg;
 
-echo check_credentials($user, $pass);
-// echo response to stdout (will be forwarded to backend)
 ?>
